706787fe318b946fc8865b7a209b6ee5a7862d97
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-07-31 10:20:24 +0530

    Selenium Season- 1 Notes and Programs till july 31st

---
+++ src/jul31st/LinksTesting1.java
@@ -0,0 +1,30 @@
+package jul31st;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LinksTesting1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://adactinhotelapp.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.linkText("Forgot Password?")).click();
+		
+		Thread.sleep(3000);
+		
+		System.out.println(driver.getTitle());
+		
+		System.out.println(driver.findElement(By.className("login_title")).isDisplayed());
+
+		System.out.println(driver.findElement(By.id("Submit")).isEnabled());
+		
+		driver.quit();
+	}
+
+}
