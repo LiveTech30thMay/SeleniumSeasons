e6a80e695f2671fa66d715e44e35441069a334f1
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-08-08 10:47:49 +0530

    Selenium Season- 2 Notes and Programs till Aug 9\th

---
+++ src/aug8th/AdactinApp.java
@@ -0,0 +1,13 @@
+package aug8th;
+
+import org.testng.annotations.Test;
+
+public class AdactinApp {
+	
+	@Test
+	public void validateLoginTest()
+	{
+		System.out.println("Login test successfull..");
+	}
+
+}
